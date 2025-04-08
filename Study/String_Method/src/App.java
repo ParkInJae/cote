472f37ceef3de7707e8cdcab60d4e732b5b9e111
InJae Park <pak293100@jbnu.ac.kr>
2025-04-08 22:38:25 +0900

    String_method

---
+++ Study/String_Method/src/App.java
@@ -0,0 +1,30 @@
+public class App {
+/*
+    String_Method()  종류 
+     char	charAt(int index)	특정 위치의 문자를 리턴합니다.
+    boolean	equals(Object anObject)	두 문자열을 비교합니다.
+    byte[]	getBytes()	byte[]로 리턴합니다.
+    byte[]	getBytes(Charset charset)	주어진 문자셋으로 인코딩한 byte[]로 리턴합니다.
+    int	indexOf(String str)	문자열 내에서 주어진 문자열의 위치를 리턴합니다.
+    int	length()	총 문자의 수를 리턴합니다.
+    String	replace(CharSequence target, CharSequence replacement)	target 부분을 replacement로 대치한 새로운 문자열을 리턴합니다.
+    String	substring(int beginIndex)	beginIndex 위치에서 끝까지 잘라낸 새로운 문자열을 리턴합니다.
+    String	substring(int beginIndex, int endIndex)	beginIndex 위치에서 endIndex 전까지 잘라낸 새로운 문자열을 리턴합니다.
+    String	toLowerCase()	알파벳 소문자로 변환한 새로운 문자열을 리턴합니다.
+    String	toUpperCase()	알파벳 대문자로 변환한 새로운 문자열을 리턴합니다.
+    String	trim()	앞뒤 공백을 제거한 새로운 문자열을 리턴합니다.
+    String	valueOf(int i)
+    valueOf(double d)	기본 타입 값을 문자열로 리턴합니다.
+ */
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Hello, World!");
+
+        
+    }
+
+
+
+
+}
